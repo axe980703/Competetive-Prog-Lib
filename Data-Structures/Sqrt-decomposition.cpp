022420343ec90bca3f0cb4c75108e69b15bf4952
Kurban <axe980703@gmail.com>
2022-04-10 19:53:27 +0500

    Update Sqrt-decomposition.cpp

--- Data-Structures/Sqrt-decomposition.cpp
+++ Data-Structures/Sqrt-decomposition.cpp
@@ -1,93 +1,59 @@
-class sqrt_dec {
+class sqrt_dec_max_min {
     int len, n;
-    vector<int> blc, a;
+    vector<int> blc_min, blc_max, arr;
 
 public:
-    sqrt_dec(int a[], int n) {
+    sqrt_dec_max_min(int a[], int n) {
         len = int(sqrt(n) + 1);
-        blc = vector<int>(len);
-        this->a.assign(a, a + n);
+        blc_min = vector<int>(len, INF);
+      	blc_max = vector<int>(len, 0);
+        arr.assign(a, a + n);
         this->n = n;
     }
-
-    void pre_sum() {
-        for (int i = 0; i < n; i++)
-            blc[i / len] += a[i];
-    }
-
-    int get_sum(int l, int r) {
-        int c_l = l / len, c_r = r / len, end = (c_l + 1) * len - 1;
-        int sum = 0;
-        if (c_l == c_r) {
-            for (int i = l; i <= r; i++)
-                sum += a[i];
-        } else {
-            for (int i = l; i <= end; i++)
-                sum += a[i];
-            for (int i = c_l + 1; i <= c_r - 1; i++)
-                sum += blc[i];
-            for (int i = c_r * len; i <= r; i++)
-                sum += a[i];
-        }
-        return sum;
-    }
-};
-
-
-class sqrt_dec_sum_max {
-    int len, n;
-    vector<int> blc_sum, blc_max, a_sum, a_max;
- 
-public:
-    sqrt_dec(int a[], int b[], int n) {
-        len = int(sqrt(n) + 1);
-        blc_sum = blc_max = vector<int>(len);
-        this->a_sum.assign(a, a + n);
-        this->a_max.assign(b, b + n);
-        this->n = n;
-    }
- 
-    void pre_sum() {
-        for (int i = 0; i < n; i++)
-            blc_sum[i / len] += a_sum[i];
-    }
- 
+    
     void pre_max() {
         for (int i = 0; i < n; i++) {
             int j = i / len;
-            blc_max[j] = max(blc_max[j], a_max[i]);
+            blc_max[j] = max(blc_max[j], arr[i]);
         }
     }
- 
-    int get_sum(int l, int r) {
-        int sum = 0, c_l = l / len, c_r = r / len, end = (c_l + 1) * len - 1;
-        if (c_l == c_r) {
-            for (int i = l; i <= r; i++)
-                sum += a_sum[i];
-        } else {
-            for (int i = l; i <= end; i++)
-                sum += a_sum[i];
-            for (int i = c_l + 1; i <= c_r - 1; i++)
-                sum += blc_sum[i];
-            for (int i = c_r * len; i <= r; i++)
-                sum += a_sum[i];
+
+    void pre_min() {
+        for (int i = 0; i < n; i++) {
+            int j = i / len;
+            blc_min[j] = min(blc_min[j], arr[i]);
         }
-        return sum;
     }
  
     int get_max(int l, int r) {
         int mx = 0, c_l = l / len, c_r = r / len, end = (c_l + 1) * len - 1;
         if (c_l == c_r) {
             for (int i = l; i <= r; i++)
-                mx = max(mx, a_max[i]);
+                mx = max(mx, arr[i]);
         } else {
             for (int i = l; i <= end; i++)
-                mx = max(mx, a_max[i]);
+                mx = max(mx, arr[i]);
             for (int i = c_l + 1; i <= c_r - 1; i++)
                 mx = max(mx, blc_max[i]);
             for (int i = c_r * len; i <= r; i++)
-                mx = max(mx, a_max[i]);
+                mx = max(mx, arr[i]);
         }
         return mx;
+    }
+
+    int get_min(int l, int r) {
+        int mn = INF, c_l = l / len, c_r = r / len, end = (c_l + 1) * len - 1;
+        if (c_l == c_r) {
+            for (int i = l; i <= r; i++)
+                mn = min(mn, arr[i]);
+        } else {
+            for (int i = l; i <= end; i++)
+                mn = min(mn, arr[i]);
+            for (int i = c_l + 1; i <= c_r - 1; i++)
+                mn = min(mn, blc_min[i]);
+            for (int i = c_r * len; i <= r; i++)
+                mn = min(mn, arr[i]);
+        }
+        return mn;
     }
 };
